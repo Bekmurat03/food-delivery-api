888c4967d87f6ae6b5c5d6aca4d9a8700eef3c46
Bekmurat <47941748+Bekmurat03@users.noreply.github.com>
2025-06-13 23:48:57 +0500

    Add expo-app-js/src/translations/ru.js 1749840517534

---
+++ expo-app-js/src/translations/ru.js
@@ -0,0 +1,45 @@
+export default {
+  // Auth screens
+  login_title: 'Вход',
+  register_title: 'Регистрация',
+  username: 'Имя пользователя',
+  password: 'Пароль',
+  email: 'Электронная почта',
+  login_button: 'Войти',
+  register_button: 'Зарегистрироваться',
+  forgot_password: 'Забыли пароль?',
+  no_account: 'Нет аккаунта?',
+  have_account: 'Уже есть аккаунт?',
+
+  // Home screen
+  home_title: 'Доставка еды',
+  search_placeholder: 'Поиск ресторанов и блюд',
+  categories: 'Категории',
+  popular: 'Популярное',
+  all_restaurants: 'Все рестораны',
+  
+  // Restaurant items
+  free_delivery: 'Бесплатная доставка',
+  min_order: 'Мин. заказ',
+  delivery_time: 'Время доставки',
+  rating: 'Рейтинг',
+  
+  // Categories
+  pizza: 'Пицца',
+  sushi: 'Суши',
+  burger: 'Бургеры',
+  asian: 'Азиатская кухня',
+  desserts: 'Десерты',
+  
+  // Cart
+  cart: 'Корзина',
+  cart_empty: 'Ваша корзина пуста',
+  total: 'Итого',
+  checkout: 'Оформить заказ',
+  
+  // Common
+  loading: 'Загрузка...',
+  error: 'Произошла ошибка',
+  try_again: 'Попробовать снова',
+  currency: '₽'
+};
